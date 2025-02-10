1e5added23c0a44f268589b7818636b9c1b125d5
babamurad <babamurad2010@yandex.ru>
2025-02-10 15:13:22 +0500

    Document index component

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Livewire\Category\CategoryIndex;
 use App\Livewire\Department\DepartmentCreate;
 use App\Livewire\Department\DepartmentEdit;
 use App\Livewire\Department\DepartmentIndex;
+use App\Livewire\Documents\DocumentsCreate;
+use App\Livewire\Documents\DocumentsEdit;
+use App\Livewire\Documents\DocumentsIndex;
+use App\Livewire\Documents\DocumentsView;
 use App\Livewire\HomeComponent;
 use App\Livewire\User\Login;
 use App\Livewire\User\Register;
@@ -21,6 +25,11 @@ Route::middleware('auth')->group(function (){
     Route::get('category', CategoryIndex::class)->name('category');
     Route::get('category/create', CategoryCreate::class)->name('category.create');
     Route::get('category/{id}/edit', CategoryEdit::class)->name('category.edit');
+
+    Route::get('documents',DocumentsIndex::class)->name('documents');
+    Route::get('documents/create', DocumentsCreate::class)->name('documents.create');
+    Route::get('documents/{id}/edit', DocumentsEdit::class)->name('documents.edit');
+    Route::name('documents/{id}/view',DocumentsView::class)->name('document.view');
 });
 
 Route::middleware('guest')->group(function (){
