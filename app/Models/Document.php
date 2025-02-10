20eea0efb35ef00decd7dbc5a2d222aaa69e5b6b
babamurad <babamurad2010@yandex.ru>
2025-02-10 14:12:31 +0500

    Document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'description', 'category_id', 'department_id', 'user_id', 'file', 'type', 'is_published', 'status_id', 'comment', 'approved_at', 'rejected_at', 'submitted_at', 'published_at', 'archived_at'
+    ];
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,23 @@ use App\Livewire\User\Login;
 use App\Livewire\User\Register;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::middleware('auth')->group(function (){
+    Route::get('/', HomeComponent::class)->name('home');
 
-Route::get('/', HomeComponent::class)->name('home');
-Route::get('register', Register::class)->name('register');
-Route::get('login', Login::class)->name('login');
+    Route::get('department', DepartmentIndex::class)->name('department');
+    Route::get('department/create', DepartmentCreate::class)->name('department.create');
+    Route::get('department/{id}/edit', DepartmentEdit::class)->name('department.edit');
 
-Route::get('department', DepartmentIndex::class)->name('department');
-Route::get('department/create', DepartmentCreate::class)->name('department.create');
-Route::get('department/{id}/edit', DepartmentEdit::class)->name('department.edit');
+    Route::get('category', CategoryIndex::class)->name('category');
+    Route::get('category/create', CategoryCreate::class)->name('category.create');
+    Route::get('category/{id}/edit', CategoryEdit::class)->name('category.edit');
+});
 
-Route::get('category', CategoryIndex::class)->name('category');
-Route::get('category/create', CategoryCreate::class)->name('category.create');
-Route::get('category/{id}/edit', CategoryEdit::class)->name('category.edit');
+Route::middleware('guest')->group(function (){
+    Route::get('register', Register::class)->name('register');
+    Route::get('login', Login::class)->name('login');
+});
+
+
+
 
